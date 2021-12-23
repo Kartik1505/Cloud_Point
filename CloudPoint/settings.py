58194795ffaf26c99f35e3031df4a0fcc8bd9d71
Kartik1505 <kartikabuj@gmail.com>
2021-12-24 00:58:13 +0530

    sixthcommit

--- CloudPoint/settings.py
+++ CloudPoint/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', "static-root")  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
